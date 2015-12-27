92cf9203748a7485e4fb465017d814c21418915e
anzhehong <anzhehong@126.com>
2015-12-27 18:25:49 +0800

    add balance shown on bottom of wallet page

---
+++ HelloMoneyOriginalUI/Views/WalletPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using NavigationMenuSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace NavigationMenuSample.Views
+{
+    public sealed partial class WalletPage : Page
+    {
+        private List<Wallet> Wallets;
+        private double Balance = 0;
+        public WalletPage()
+        {
+            this.InitializeComponent();
+            Wallets = WalletHelper.testGet();
+            Balance = 199;
+
+            Balance = WalletHelper.getPresentBalance().Result;
+        }
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            this.Frame.Navigate(
+                typeof(BasicSubPage),
+                e.ClickedItem,
+                new Windows.UI.Xaml.Media.Animation.DrillInNavigationTransitionInfo());
+            var wallet = (Wallet)e.ClickedItem;
+
+        }
+    }
+}
