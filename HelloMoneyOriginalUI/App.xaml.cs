0362d14fb2c959ac6c1cd594f100d88a15c6d349
Chris <tjuyinsh@gmail.com>
2015-12-30 01:42:02 +0800

    TestForCortana
    test for cortana

--- HelloMoneyOriginalUI/App.xaml.cs
+++ HelloMoneyOriginalUI/App.xaml.cs
@@ -20,11 +20,13 @@ using Windows.UI.Xaml.Navigation;
 namespace NavigationMenuSample
 {
     using Models;
+    using System.Threading.Tasks;
     using Views;
-
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
+    using Windows.ApplicationModel.VoiceCommands;
+    using Windows.Media.SpeechRecognition;
+    using Windows.Storage;    /// <summary>
+                              /// Provides application-specific behavior to supplement the default Application class.
+                              /// </summary>
     sealed partial class App : Application
     {
         /// <summary>
@@ -92,7 +94,7 @@ namespace NavigationMenuSample
         /// will be used such as when the application is launched to open a specific file.
         /// </summary>
         /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        protected override async void OnLaunched(LaunchActivatedEventArgs e)
         {
 
 #if DEBUG
@@ -135,6 +137,20 @@ namespace NavigationMenuSample
 
             // Ensure the current window is active
             Window.Current.Activate();
+
+            // register voice commands
+            try
+            {
+                // Install the main VCD. Since there's no simple way to test that the VCD has been imported, or that it's your most recent
+                // version, it's not unreasonable to do this upon app load.
+                StorageFile vcdStorageFile = await Package.Current.InstalledLocation.GetFileAsync(@"VoiceCommandFile.xml");
+
+                await Windows.ApplicationModel.VoiceCommands.VoiceCommandDefinitionManager.InstallCommandDefinitionsFromStorageFileAsync(vcdStorageFile);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Installing Voice Commands Failed: " + ex.ToString());
+            }
         }
 
         /// <summary>
@@ -161,5 +177,124 @@ namespace NavigationMenuSample
             walletHelper.SaveToFile();
             deferral.Complete();
         }
+
+
+        /// <summary>
+        /// OnActivated is the entry point for an application when it is launched via
+        /// means other normal user interaction. This includes Voice Commands, URI activation,
+        /// being used as a share target from another app, etc. Here, we're going to handle the
+        /// Voice Command activation from Cortana.
+        /// 
+        /// Note: Be aware that an older VCD could still be in place for your application if you
+        /// modify it and update your app via the store. You should be aware that you could get 
+        /// activations that include commands in older versions of your VCD, and you should try
+        /// to handle them gracefully.
+        /// </summary>
+        /// <param name="args">Details about the activation method, including the activation
+        /// phrase (for voice commands) and the semantic interpretation, parameters, etc.</param>
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            base.OnActivated(args);
+
+            Type navigationToPageType = typeof(WalletPage);
+            //ViewModel.TripVoiceCommand? navigationCommand = null;
+
+            // If the app was launched via a Voice Command, this corresponds to the "show trip to <location>" command. 
+            // Protocol activation occurs when a tile is clicked within Cortana (via the background task)
+            if (args.Kind == ActivationKind.VoiceCommand)
+            {
+                // The arguments can represent many different activation types. Cast it so we can get the
+                // parameters we care about out.
+                var commandArgs = args as VoiceCommandActivatedEventArgs;
+
+                Windows.Media.SpeechRecognition.SpeechRecognitionResult speechRecognitionResult = commandArgs.Result;
+
+                // Get the name of the voice command and the text spoken. See AdventureWorksCommands.xml for
+                // the <Command> tags this can be filled with.
+                string voiceCommandName = speechRecognitionResult.RulePath[0];
+                string textSpoken = speechRecognitionResult.Text;
+
+                // The commandMode is either "voice" or "text", and it indictes how the voice command
+                // was entered by the user.
+                // Apps should respect "text" mode by providing feedback in silent form.
+                string commandMode = this.SemanticInterpretation("commandMode", speechRecognitionResult);
+
+                switch (voiceCommandName)
+                {
+                    case "showWallet":
+                        // Set the page to navigate to for this voice command.                        
+                        navigationToPageType = typeof(WalletPage);
+                        break;
+                    case "addRecord":
+                        navigationToPageType = typeof(RecordPage);
+                        break;
+                    case "showBill":
+                        navigationToPageType = typeof(BillPage);
+                        break;
+                    default:
+                        // If we can't determine what page to launch, go to the default entry point.
+                        navigationToPageType = typeof(BillPage);
+                        break;
+                }
+            }
+            else if (args.Kind == ActivationKind.Protocol)
+            {
+                // Extract the launch context. In this case, we're just using the destination from the phrase set (passed
+                // along in the background task inside Cortana), which makes no attempt to be unique. A unique id or 
+                // identifier is ideal for more complex scenarios. We let the destination page check if the 
+                // destination trip still exists, and navigate back to the trip list if it doesn't.
+                var commandArgs = args as ProtocolActivatedEventArgs;
+                Windows.Foundation.WwwFormUrlDecoder decoder = new Windows.Foundation.WwwFormUrlDecoder(commandArgs.Uri.Query);
+                var destination = decoder.GetFirstValueByName("LaunchContext");
+
+            }
+            else
+            {
+                // If we were launched via any other mechanism, fall back to the main page view.
+                // Otherwise, we'll hang at a splash screen.
+                navigationToPageType = typeof(WalletPage);
+            }
+
+            AppShell shell = Window.Current.Content as AppShell;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (shell == null)
+            {
+                // Create a AppShell to act as the navigation context and navigate to the first page
+                shell = new AppShell();
+
+                // Set the default language
+                shell.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
+
+                shell.AppFrame.NavigationFailed += OnNavigationFailed;
+            }
+
+            // Place our app shell in the current Window
+            Window.Current.Content = shell;
+
+            if (shell.AppFrame.Content == null)
+            {
+                // When the navigation stack isn't restored, navigate to the first page
+                // suppressing the initial entrance animation.
+                shell.AppFrame.Navigate(typeof(WalletPage));
+            }
+
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Returns the semantic interpretation of a speech result. Returns null if there is no interpretation for
+        /// that key.
+        /// </summary>
+        /// <param name="interpretationKey">The interpretation key.</param>
+        /// <param name="speechRecognitionResult">The result to get an interpretation from.</param>
+        /// <returns></returns>
+        private string SemanticInterpretation(string interpretationKey, SpeechRecognitionResult speechRecognitionResult)
+        {
+            return speechRecognitionResult.SemanticInterpretation.Properties[interpretationKey].FirstOrDefault();
+        }
     }
 }
+
---
+++ HelloMoneyOriginalUI/VoiceCommandService/HelloMoneyVoiceCommandService.cs
@@ -0,0 +1,203 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.Resources.Core;
+using Windows.ApplicationModel.VoiceCommands;
+
+namespace HelloMoney.VoiceCommands
+{
+    /// <summary>
+    /// The VoiceCommandService implements the entrypoint for all headless voice commands
+    /// invoked via Cortana. The individual commands supported are described in the
+    /// AdventureworksCommands.xml VCD file in the AdventureWorks project. The service
+    /// entrypoint is defined in the Package Manifest (See section uap:Extension in 
+    /// AdventureWorks:Package.appxmanifest)
+    /// </summary>
+    public sealed class HelloMoneyVoiceCommandService : IBackgroundTask
+    {
+        /// <summary>
+        /// the service connection is maintained for the lifetime of a cortana session, once a voice command
+        /// has been triggered via Cortana.
+        /// </summary>
+        VoiceCommandServiceConnection voiceServiceConnection;
+
+        /// <summary>
+        /// Lifetime of the background service is controlled via the BackgroundTaskDeferral object, including
+        /// registering for cancellation events, signalling end of execution, etc. Cortana may terminate the 
+        /// background service task if it loses focus, or the background task takes too long to provide.
+        /// 
+        /// Background tasks can run for a maximum of 30 seconds.
+        /// </summary>
+        BackgroundTaskDeferral serviceDeferral;
+
+        /// <summary>
+        /// ResourceMap containing localized strings for display in Cortana.
+        /// </summary>
+        ResourceMap cortanaResourceMap;
+
+        /// <summary>
+        /// The context for localized strings.
+        /// </summary>
+        ResourceContext cortanaContext;
+
+        /// <summary>
+        /// Get globalization-aware date formats.
+        /// </summary>
+        DateTimeFormatInfo dateFormatInfo;
+
+        /// <summary>
+        /// Background task entrypoint. Voice Commands using the <VoiceCommandService Target="...">
+        /// tag will invoke this when they are recognized by Cortana, passing along details of the 
+        /// invocation. 
+        /// 
+        /// Background tasks must respond to activation by Cortana within 0.5 seconds, and must 
+        /// report progress to Cortana every 5 seconds (unless Cortana is waiting for user
+        /// input). There is no execution time limit on the background task managed by Cortana,
+        /// but developers should use plmdebug (https://msdn.microsoft.com/en-us/library/windows/hardware/jj680085%28v=vs.85%29.aspx)
+        /// on the Cortana app package in order to prevent Cortana timing out the task during
+        /// debugging.
+        /// 
+        /// Cortana dismisses its UI if it loses focus. This will cause it to terminate the background
+        /// task, even if the background task is being debugged. Use of Remote Debugging is recommended
+        /// in order to debug background task behaviors. In order to debug background tasks, open the
+        /// project properties for the app package (not the background task project), and enable
+        /// Debug -> "Do not launch, but debug my code when it starts". Alternatively, add a long
+        /// initial progress screen, and attach to the background task process while it executes.
+        /// </summary>
+        /// <param name="taskInstance">Connection to the hosting background service process.</param>
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            serviceDeferral = taskInstance.GetDeferral();
+
+            // Register to receive an event if Cortana dismisses the background task. This will
+            // occur if the task takes too long to respond, or if Cortana's UI is dismissed.
+            // Any pending operations should be cancelled or waited on to clean up where possible.
+            taskInstance.Canceled += OnTaskCanceled;
+
+            var triggerDetails = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+
+            // Load localized resources for strings sent to Cortana to be displayed to the user.
+            cortanaResourceMap = ResourceManager.Current.MainResourceMap.GetSubtree("Resources");
+
+            // Select the system language, which is what Cortana should be running as.
+            cortanaContext = ResourceContext.GetForViewIndependentUse();
+
+            // Get the currently used system date format
+            dateFormatInfo = CultureInfo.CurrentCulture.DateTimeFormat;
+
+            // This should match the uap:AppService and VoiceCommandService references from the 
+            // package manifest and VCD files, respectively. Make sure we've been launched by
+            // a Cortana Voice Command.
+            if (triggerDetails != null && triggerDetails.Name == "HelloMoneyVoiceCommandService")
+            {
+                try
+                {
+                    voiceServiceConnection =
+                        VoiceCommandServiceConnection.FromAppServiceTriggerDetails(
+                            triggerDetails);
+
+                    voiceServiceConnection.VoiceCommandCompleted += OnVoiceCommandCompleted;
+
+                    // GetVoiceCommandAsync establishes initial connection to Cortana, and must be called prior to any 
+                    // messages sent to Cortana. Attempting to use ReportSuccessAsync, ReportProgressAsync, etc
+                    // prior to calling this will produce undefined behavior.
+                    VoiceCommand voiceCommand = await voiceServiceConnection.GetVoiceCommandAsync();
+
+                    // Depending on the operation (defined in AdventureWorks:AdventureWorksCommands.xml)
+                    // perform the appropriate command.
+                    switch (voiceCommand.CommandName)
+                    {
+                        default:
+                            // As with app activation VCDs, we need to handle the possibility that
+                            // an app update may remove a voice command that is still registered.
+                            // This can happen if the user hasn't run an app since an update.
+                            LaunchAppInForeground();
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine("Handling Voice Command failed " + ex.ToString());
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Show a progress screen. These should be posted at least every 5 seconds for a 
+        /// long-running operation, such as accessing network resources over a mobile 
+        /// carrier network.
+        /// </summary>
+        /// <param name="message">The message to display, relating to the task being performed.</param>
+        /// <returns></returns>
+        private async Task ShowProgressScreen(string message)
+        {
+            var userProgressMessage = new VoiceCommandUserMessage();
+            userProgressMessage.DisplayMessage = userProgressMessage.SpokenMessage = message;
+
+            VoiceCommandResponse response = VoiceCommandResponse.CreateResponse(userProgressMessage);
+            await voiceServiceConnection.ReportProgressAsync(response);
+        }
+
+        /// <summary>
+        /// Provide a simple response that launches the app. Expected to be used in the
+        /// case where the voice command could not be recognized (eg, a VCD/code mismatch.)
+        /// </summary>
+        private async void LaunchAppInForeground()
+        {
+            var userMessage = new VoiceCommandUserMessage();
+            userMessage.SpokenMessage = cortanaResourceMap.GetValue("LaunchingHelloMoney", cortanaContext).ValueAsString;
+
+            var response = VoiceCommandResponse.CreateResponse(userMessage);
+
+            response.AppLaunchArgument = "";
+
+            await voiceServiceConnection.RequestAppLaunchAsync(response);
+        }
+
+        /// <summary>
+        /// Handle the completion of the voice command. Your app may be cancelled
+        /// for a variety of reasons, such as user cancellation or not providing 
+        /// progress to Cortana in a timely fashion. Clean up any pending long-running
+        /// operations (eg, network requests).
+        /// </summary>
+        /// <param name="sender">The voice connection associated with the command.</param>
+        /// <param name="args">Contains an Enumeration indicating why the command was terminated.</param>
+        private void OnVoiceCommandCompleted(VoiceCommandServiceConnection sender, VoiceCommandCompletedEventArgs args)
+        {
+            if (this.serviceDeferral != null)
+            {
+                this.serviceDeferral.Complete();
+            }
+        }
+
+        /// <summary>
+        /// When the background task is cancelled, clean up/cancel any ongoing long-running operations.
+        /// This cancellation notice may not be due to Cortana directly. The voice command connection will
+        /// typically already be destroyed by this point and should not be expected to be active.
+        /// </summary>
+        /// <param name="sender">This background task instance</param>
+        /// <param name="reason">Contains an enumeration with the reason for task cancellation</param>
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            System.Diagnostics.Debug.WriteLine("Task cancelled, clean up");
+            if (this.serviceDeferral != null)
+            {
+                //Complete the service deferral
+                this.serviceDeferral.Complete();
+            }
+        }
+    }
+}
